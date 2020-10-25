8e29adcd0d3a84846472359c640a393e461c4ac0
spencer-bao <spencerbao@gmail.com>
2020-10-25 19:38:34 -0400

    created gui and layout files

---
+++ app/src/main/java/edu/wm/cs/cs301/SpencerBao/gui/AMazeActivity.java
@@ -0,0 +1,15 @@
+package edu.wm.cs.cs301.SpencerBao.gui;
+
+import android.os.Bundle;
+
+import com.example.amazebyspencerbao.R;
+
+public class AMazeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
---
+++ app/src/main/java/edu/wm/cs/cs301/SpencerBao/gui/LosingActivity.java
@@ -0,0 +1,4 @@
+package edu.wm.cs.cs301.SpencerBao.gui;
+
+public class LosingActivity {
+}
